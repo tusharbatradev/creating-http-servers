25ab01d47c29504c1ce476b41356e7dba09c2c5a
Tushar Batra <tusharbatra08@gmail.com>
2025-02-16 00:24:07 +0530

    Studied more about the NodeJs

--- index.js
+++ index.js
@@ -1,14 +1,72 @@
 const express = require("express");
+const users = require("./MOCK_DATA.json");
+const fs = require("fs");
 
 const app = express();
+const port = 8000;
 
-app.get('/' , (req,res) => {
-    res.send('Hello World')
+app.use(express.urlencoded({ extended: false }));
+
+// Routes
+app.get("/users", (req, res) => {
+  const html = `<ul> 
+      ${users.map((user) => `<li>${user.first_name}</li>`).join("")}
+      </ul>`;
+
+  res.send(html);
 });
 
-app.get('/about', (req, res) => {
-    res.send(`Hello ${req.query.name}`)
-})
+app
+  .route("/api/users/:id")
+  .get((req, res) => {
+    const id = Number(req.params.id);
+    console.log("Requested ID:", id);
 
-const port = 8000
-app.listen(port, console.log(`Server started on ${port}`))
+    const user = users.find((user) => user.id === id);
+
+    if (!user) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    res.json(user);
+  })
+  .put((req, res) => {
+    const id = Number(req.params.id);
+    console.log("Requested ID:", id);
+
+    // User find karo
+    const userIndex = id - 1;
+
+    users[userIndex] = { ...users[userIndex], ...req.body };
+
+    res.json({ message: "User updated successfully", user: users[userIndex] });
+  })
+  .delete((req, res) => {
+    const id = Number(req.params.id);
+    console.log("Requested ID:", id);
+
+    const deleteUser = users.splice(id - 1, 1);
+
+    res.json(deleteUser);
+  });
+
+// Get all users
+app.get("/api/users", (req, res) => {
+  res.json(users);
+});
+
+// Post
+app.post("/api/users", (req, res) => {
+  let body = req.body;
+
+  // Generate new ID
+  let newUser = { ...body, id: users.length + 1 };
+  users.push(newUser);
+  fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), (err, data) => {
+    return res.json({ status: "User Added" });
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
